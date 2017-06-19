564955a0a80be8ac29a67f6fa050d3e93c02c475
Christian Auth <c.auth@syzygy.de>
2017-06-19 16:31:42 +0200

    fix: removes comma

--- source/index.js
+++ source/index.js
@@ -5,5 +5,5 @@ module.exports =
 {
     linter: require('./linter/index.js'),
     model: require('./model/index.js'),
-    parser: require('./parser/index.js'),
+    parser: require('./parser/index.js')
 };
