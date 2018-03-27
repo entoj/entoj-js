020f5bb0bc395bc5812afe3accf5242a2645d4e8
Christian Auth <c.auth@syzygy.de>
2018-03-27 14:58:01 +0200

    feat(task.PostProcessJsTask): allow more control over specific uglifyjs options

--- source/task/PostProcessJsTask.js
+++ source/task/PostProcessJsTask.js
@@ -104,16 +104,108 @@ class PostProcessJsTask extends TransformingTask
                 },
                 warnings: true
             };
-            if (params.optimize)
+            if (params.minify)
             {
+                /*
+                //Defaults
                 options.compress =
                 {
-                    reduce_vars: true,
-                    join_vars: true,
-                    dead_code: true,
+                    'arguments': true,
+                    booleans: true,
+                    collapse_vars: true,
+                    comparisons: true,
                     conditionals: true,
-                    unused: true
+                    dead_code: true,
+                    drop_console: false,
+                    drop_debugger: true,
+                    evaluate: true,
+                    expression: false,
+                    hoist_funs: false,
+                    hoist_props: true,
+                    hoist_vars: false,
+                    if_return: true,
+                    inline: true,
+                    join_vars: true,
+                    keep_fargs: true,
+                    reduce_vars: true,
+                    sequences: true,
+                    side_effects: true,
+                    switches: true,
+                    toplevel: false,
+                    top_retain: null,
+                    typeofs: true,
+                    unsafe: false,
+                    unsafe_comps: false,
+                    unsafe_Function: false,
+                    unsafe_math: false,
+                    unsafe_proto: false,
+                    unsafe_regexp: false,
+                    unsafe_undefined: false,
+                    unused: true,
+                    warnings: false
                 };
+                */
+                // Disable all plugins
+                options.compress =
+                {
+                    'arguments': false,
+                    booleans: false,
+                    collapse_vars: false,
+                    comparisons: false,
+                    conditionals: false,
+                    dead_code: false,
+                    drop_console: false,
+                    drop_debugger: false,
+                    evaluate: false,
+                    expression: false,
+                    hoist_funs: false,
+                    hoist_props: false,
+                    hoist_vars: false,
+                    if_return: false,
+                    inline: false,
+                    join_vars: false,
+                    keep_fargs: false,
+                    reduce_vars: false,
+                    sequences: false,
+                    side_effects: false,
+                    switches: false,
+                    toplevel: false,
+                    top_retain: null,
+                    typeofs: false,
+                    unsafe: false,
+                    unsafe_comps: false,
+                    unsafe_Function: false,
+                    unsafe_math: false,
+                    unsafe_proto: false,
+                    unsafe_regexp: false,
+                    unsafe_undefined: false,
+                    unused: false,
+                    warnings: false
+                };
+
+                // Enable specific plugins
+                options.compress = Object.assign(options.compress,
+                    {
+                        'arguments': true,
+                        booleans: true,
+                        collapse_vars: true,
+                        comparisons: true,
+                        conditionals: true,
+                        dead_code: true,
+                        drop_debugger: true,
+                        evaluate: true,
+                        hoist_props: true,
+                        if_return: true,
+                        inline: true,
+                        join_vars: true,
+                        keep_fargs: true,
+                        reduce_vars: false,
+                        sequences: true,
+                        side_effects: true,
+                        switches: true,
+                        typeofs: true,
+                        unused: true
+                    }, (typeof options.compress == 'object') ? options.compress : {});
             }
             if (params.sourceMaps)
             {
